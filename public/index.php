7c6651a409defa36906427a10d1c75ab1d12ec01
Erik <kallgrenerik@gmail.com>
2023-02-20 19:11:29 +0100

    navstate-rewrite

--- public/index.php
+++ public/index.php
@@ -1,42 +1,21 @@
 <?php
 
-if (!isset($_GET["page"])) {
-    $_GET["page"] = "home";
+if ($_SERVER['REQUEST_URI'] === "/") {
+    $url = "/home";
+}
+else {
+    $url = $_SERVER['REQUEST_URI'];
 }
 
-if ($_GET["page"] === "home") {
-    ?>
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Home</title>
-        </head>
-        <body>
-            <h1>Home</h1>
 
-            <ul>
-                <li><a href="?page=home">Home</a></li>
-                <li><a href="?page=blog">Blog</a></li>
-                <li><a href="?page=about">About</a></li>
-            </ul>
-
-            <div style="width: 200px; height: 200px; background-color: red;">
-
-            </div>
-
-        </body>
-        </html>
-    <?php
+if ($url === "/home") {
+    require "home.html";
 }
-if ($_GET["page"] === "blog") {
+if ($url === "/blog") {
     require "blog.html";
 }
-if ($_GET["page"] === "about") {
+if ($url === "/about") {
     require "about.html";
 }
 
-
-?>
+// die($_SERVER['REQUEST_URI']);
